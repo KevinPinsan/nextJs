5ef77d8e5819a23fce66f9d19ae961fee61d8cdc
kevin <64093935+KevinPinsan@users.noreply.github.com>
2020-11-21 20:11:21 -0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -26,50 +26,135 @@ export default function Home() {
       </header>
 
       <main className={styles.main}>
-        <div className={styles.slider}>
-          <h1>Slider</h1>
+        <div className={styles.slider }>
+        
         </div>
         <div>
           <h1>
             Serviços
           </h1>
         </div>
-        <div>
-          <section>
-            <div>
-              <p>imagem e lista de serviços</p>
-            </div>
-            <div>
-              <p>imagem e lista de serviços</p>
-            </div>
-            <div>
-              <p>imagem e lista de serviços</p>
-            </div>
+        <div className={styles.services}>
+          <section className={styles.section}>
+              <div className={styles.card}> 
+                <div className={styles.cardImage}>
+                  imagem
+                </div>
+                <div className={styles.cardText}>
+                  <h3>
+                    Martelinho de Ouro
+                  </h3>
+                  <ul>
+                    <li>
+                      Vincos
+                    </li>
+                    <li>
+                      Chuva de Granizo
+                    </li>
+                    <li>
+                      Amassados pequeno e grande porte
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className={styles.card}> 
+                <div className={styles.cardImage}>
+                  imagem
+                </div>
+                <div className={styles.cardText}> 
+                  <h3>
+                    Funilaria Express
+                  </h3>
+                  <ul>
+                    <li>
+                      Retoques
+                    </li>
+                    <li>
+                      Serviços rapidos
+                    </li>
+                    <li>
+                      Pequenos amassados
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className={styles.card}> 
+                <div className={styles.cardImage}>
+                  imagem
+                </div>
+                <div className={styles.cardText}>
+                  <h3>
+                    Polimento
+                  </h3>
+                  <ul>
+                    <li>
+                      Polimento geral
+                    </li>
+                    <li>
+                      Polimento farol
+                    </li>
+                    <li>
+                      Remoção de riscos
+                    </li>
+                  </ul>
+                </div>
+              </div>
           </section>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <ul>
+        <div className={styles.containerFooter}>
+          <div>
+            <h1>
+              Endereço
+            </h1>
+            <ul>
+              <li>
+                Rua central, 530 - <bold>Cep: 01230-032</bold>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h1>
+              Contato
+            </h1>
+            <ul>
+              <li>
+                Tel: (11)4152-3040
+              </li>
+              <li>
+                Cel: (11)9.8833-0455
+              </li>
+              <li>
+                suaoficina@gmail.com
+              </li>
+            </ul>
+          </div>
+          <div> 
+            <h1>
+              Atendimento
+            </h1>
+            <ul>
+              <li>
+                Seg - Sex: 08hr00 - 18hr00
+              </li>
+              <li>
+                Sab: 08hr00 - 13hr00
+              </li>
+              <li>
+                Dom - Feriado: Fechado
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.socialMedia}>
           <ul>
-            <h1>Siga-nos</h1>
-            <li>facebook</li>
-            <li>tweeter</li>
-            <li>instagram</li>
+            <li>Facebook</li>
+            <li>Instagram</li>
+            <li>twitter</li>
           </ul>
-          <ul>
-            <h1>Atendimento</h1>
-            <li>Segunda - sexta</li>
-            <li>Sabado</li>
-            <li>Dom e Feriados</li>
-          </ul>
-          <ul>
-            <h1>Endereço</h1>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-        </ul>
+        </div>
       </footer>
     </div>
   )
